1da409a47dc262b6df8d8bd616d313635c1f91fb
Aditya <adityananaware5889@gmail.com>
2025-01-16 10:54:06 +0530

    initial commit

--- src/main/java/aditya/quiz/QuizDemoApplication.java
+++ src/main/java/aditya/quiz/QuizDemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VuQuizDemoApplication {
+public class QuizDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VuQuizDemoApplication.class, args);
+        SpringApplication.run(QuizDemoApplication.class, args);
     }
 }
